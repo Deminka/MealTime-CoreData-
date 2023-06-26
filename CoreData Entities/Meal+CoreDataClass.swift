fd2a8ae4adf5327f34ade0c292fce248cf2004ca
Dmitry Komissarov <komdimkom@gmail.com>
2023-06-26 19:42:41 +0300

    Initial Commit

---
+++ CoreData Entities/Meal+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Meal+CoreDataClass.swift
+//  MealTime
+//
+//  Created by mac on 26.06.2023.
+//  Copyright © 2023 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Meal)
+public class Meal: NSManagedObject {
+
+}
